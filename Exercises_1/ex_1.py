611df61b5229d2af52167b3488b0a72fe1f0bd8b
“yoiuremail” <mohammad@souqalmal.com>
2021-12-25 23:29:20 +0200

    add hungman

--- Exercises_1/ex_1.py
+++
@@ -1,7 +0,0 @@
-# Implement a function that takes two integers as a parameter and returs a string with the sum of the two numbers.
-
-def calculate_sum(a,b):
-      sum=a+b
-      return ('{0} + {1} = {2}'.format(a, b, sum))
-
-print(calculate_sum(3,4))
